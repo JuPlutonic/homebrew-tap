42cc3cde1bf72bb47d6b4e4470f1e61b73769b78
azhevlakov <azhevlakov@ceki.ru>
2021-04-16 22:18:33 +0300

    Dns-over-https lint

--- Formula/dns-over-https.rb
+++ Formula/dns-over-https.rb
@@ -1,33 +1,32 @@
-require "language/go"
+require 'language/go'
 
 class DnsOverHttps < Formula
-  desc "High performance DNS over HTTPS client & server"
-  homepage "https://github.com/m13253/dns-over-https"
-  head "https://github.com/m13253/dns-over-https.git"
-  url "https://github.com/m13253/dns-over-https/archive/v2.2.5.tar.gz"
-  sha256 "2e7464195975619e1c2745195a3506e423eca9ccddb2234fd4b2ad9c17abb025"
+  desc 'High performance DNS over HTTPS client & server'
+  homepage 'https://github.com/m13253/dns-over-https'
+  head 'https://github.com/m13253/dns-over-https.git'
+  url 'https://github.com/m13253/dns-over-https/archive/v2.2.5.tar.gz'
+  sha256 '2e7464195975619e1c2745195a3506e423eca9ccddb2234fd4b2ad9c17abb025'
 
-
-  conflicts_with "doh-proxy", because: "both install binaries `doh-proxy`, `doh-client`"
-  depends_on "go" => :build
+  conflicts_with 'doh-proxy', because: 'both install binaries `doh-proxy`, `doh-client`'
+  depends_on 'go' => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    path = buildpath/"github.com/m13253/dns-over-https"
-    path.install Dir["*"]
+    ENV['GOPATH'] = buildpath
+    path = buildpath/'github.com/m13253/dns-over-https'
+    path.install Dir['*']
 
     cd path do
-      # the Makefile handles dependencies itself
-      system "make"
+      # The Makefile handles dependencies itself
+      system 'make'
 
-      (etc/"dns-over-https").mkpath
+      (etc/'dns-over-https').mkpath
 
-      # https://stackoverflow.com/questions/690794/ruby-arrays-w-vs-w
+      # Rubocop:disable Style/RedundantCapitalW
       %W[
         doh-client
         doh-server
       ].each do |i|
-        bin.install("#{i}/#{i}" => "#{i}")
+        bin.install("#{i}/#{i}" => i.to_s)
         etc.install("#{i}/#{i}.conf" => "dns-over-https/#{i}.conf")
       end
 
@@ -36,10 +35,9 @@ class DnsOverHttps < Formula
   end
 
   test do
-    system bin/"doh-client", "-version"
+    system bin/'doh-client', '-version'
   end
 
   # TODO: run multiple plists under one formula #192
   #   https://github.com/Homebrew/homebrew-services/issues/192
-
 end
