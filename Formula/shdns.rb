3dfad1cf1cdcd2c72d502c913a98183a98610946
azhevlakov <azhevlakov@ceki.ru>
2021-05-12 03:14:38 +0300

    Upd/corrested shdns

--- Formula/shdns.rb
+++ Formula/shdns.rb
@@ -1,22 +1,22 @@
 require "language/go"
 
 class Shdns < Formula
-  desc "A port of ChinaDNS (DNS filter) in golang with IPv6 support"
+  desc "Port of ChinaDNS (DNS filter) in golang with IPv6 support"
   homepage "https://github.com/domosekai/shdns"
-  url "https://github.com/domosekai/shdns/archive/v0.8.3.tar.gz"
-  sha256 "c438f6978904f492c948af04acbe8a00a3e5c81777be8636f203507ddbf25837"
+  url "https://github.com/domosekai/shdns/archive/v0.8.4.tar.gz"
+  sha256 "01be102d4d971598c77864f87328631392c1890416a5a36c26cdbdc4470a8837"
   head "https://github.com/domosekai/shdns.git"
-
-  conflicts_with "shdns", :because => "both install `shdns` binaries"
 
   bottle :unneeded
 
   depends_on "go" => :build
 
+  conflicts_with "shdns", because: "both install `shdns` binaries"
+
   go_resource "golang.org/x/net" do
     # https://go.googlesource.com/net.git/+/refs/heads/release-branch.go1.15
     url "https://go.googlesource.com/net.git",
-      :revision => "ab34263943818b32f575efc978a3d24e80b04bd7"
+      revision: "ab34263943818b32f575efc978a3d24e80b04bd7"
   end
 
   def install
@@ -36,63 +36,64 @@ class Shdns < Formula
     end
   end
 
+  def caveats
+    <<~EOS
+      It's not recommended to run shdns alone. A forwarding DNS server
+      with cache support, like dnsmasq or unbound, should be put before it.
+
+      Caveat: port 5353 is taken by mDNSResponder. Shdns runs on
+      localhost (127.0.0.1), port 5300, balancing traffic across a set of resolvers.
+      If you would like to change these settings, edit the plist service file.
+
+      Homebrew services are run as LaunchAgents by current user.
+      To make chinadns service work on privileged port, like port 53,
+      you need to run it as a "global" daemon in /Library/LaunchAgents.
+
+        sudo cp -f #{plist_path} /Library/LaunchAgents/
+
+      Dont' use `sudo brew services`. This very command will ruin the file perms.
+    EOS
+  end
+
+  plist_options manual: "shdns -l4 /usr/local/etc/shdns/cnipv4.txt -l6 /usr/local/etc/shdns/cnipv4.txt -d 114.114.114.114,114.114.115.115 -f 208.67.222.222:443,208.67.222.220:443 -t -w 40 -b 127.0.0.1:5300"
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/shdns</string>
+            <string>-l4</string>
+            <string>#{etc}/shdns/cnipv4.txt</string>
+            <string>-l6</string>
+            <string>#{etc}/shdns/cnipv6.txt</string>
+            <string>-d</string>
+            <string>114.114.114.114,114.114.115.115</string>
+            <string>-f</string>
+            <string>208.67.222.222:443,208.67.222.220:443</string>
+            <string>-t</string>
+            <string>-w</string>
+            <string>40</string>
+            <string>-b</string>
+            <string>127.0.0.1:5300</string>
+          </array>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>RunAtLoad</key>
+          <true/>
+        </dict>
+      </plist>
+    EOS
+  end
+
   test do
     system bin/"shdns", "-h"
-  end
-
-  def caveats; <<~EOS
-    It's not recommended to run shdns alone. A forwarding DNS server
-    with cache support, like dnsmasq or unbound, should be put before it.
-
-    Caveat: port 5353 is taken by mDNSResponder. Shdns runs on
-    localhost (127.0.0.1), port 5300, balancing traffic across a set of resolvers.
-    If you would like to change these settings, edit the plist service file.
-
-    Homebrew services are run as LaunchAgents by current user.
-    To make chinadns service work on privileged port, like port 53,
-    you need to run it as a "global" daemon in /Library/LaunchAgents.
-
-      sudo cp -f #{plist_path} /Library/LaunchAgents/
-
-    Dont' use `sudo brew services`. This very command will ruin the file perms.
-  EOS
-  end
-
-  plist_options :manual => "shdns -l4 /usr/local/etc/shdns/cnipv4.txt -l6 /usr/local/etc/shdns/cnipv4.txt -d 114.114.114.114,114.114.115.115 -f 208.67.222.222:443,208.67.222.220:443 -t -w 40 -b 127.0.0.1:5300"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/shdns</string>
-          <string>-l4</string>
-          <string>#{etc}/shdns/cnipv4.txt</string>
-          <string>-l6</string>
-          <string>#{etc}/shdns/cnipv6.txt</string>
-          <string>-d</string>
-          <string>114.114.114.114,114.114.115.115</string>
-          <string>-f</string>
-          <string>208.67.222.222:443,208.67.222.220:443</string>
-          <string>-t</string>
-          <string>-w</string>
-          <string>40</string>
-          <string>-b</string>
-          <string>127.0.0.1:5300</string>
-        </array>
-        <key>KeepAlive</key>
-        <dict>
-          <key>SuccessfulExit</key>
-          <false/>
-        </dict>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-    </plist>
-    EOS
   end
 end
